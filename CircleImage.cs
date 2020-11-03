85ea05efe6ea99b0d27af06a6f23a0b23857d75d
Wencc <617186247@qq.com>
2020-11-03 19:54:05 +0800

    Add files via upload
    add circle iamge script

---
+++ CircleImage.cs
@@ -0,0 +1,328 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Sprites;
+using System;
+using System.Collections;
+using UnityEngine.Serialization;
+
+//[AddComponentMenu("UI/Circle Image")]
+public class CircleImage : MaskableGraphic, ISerializationCallbackReceiver, ILayoutElement, ICanvasRaycastFilter
+{
+
+    [FormerlySerializedAs("m_Frame")]
+    [SerializeField]
+    private Sprite m_Sprite;
+    public Sprite sprite { get { return m_Sprite; } set { if (SetPropertyUtilityExt.SetClass(ref m_Sprite, value)) SetAllDirty(); } }
+
+    [NonSerialized]
+    private Sprite m_OverrideSprite;
+    public Sprite overrideSprite { get { return m_OverrideSprite == null ? sprite : m_OverrideSprite; } set { if (SetPropertyUtilityExt.SetClass(ref m_OverrideSprite, value)) SetAllDirty(); } }
+
+    [Header("圆形或扇形填充比例")]
+    [Range(0, 1)]
+    public float fillPercent = 1f;
+    [Header("是否填充圆形")]
+    public bool fill = true;
+    [Header("圆环宽度")]
+    public float thickness = 5;
+    [Header("圆形精度")]
+    [Range(3, 100)]
+    public int segements = 20;
+
+    private List<Vector3> innerVertices;
+    private List<Vector3> outterVertices;
+
+    // Use this for initialization
+    void Awake()
+    {
+        innerVertices = new List<Vector3>();
+        outterVertices = new List<Vector3>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.thickness = (float)Mathf.Clamp(this.thickness, 0, rectTransform.rect.width / 2);
+    }
+
+
+
+    protected override void OnPopulateMesh(VertexHelper vh)
+    {
+        vh.Clear();
+
+        innerVertices.Clear();
+        outterVertices.Clear();
+
+        float degreeDelta = (float)(2 * Mathf.PI / segements);
+        int curSegements = (int)(segements * fillPercent);
+
+        float tw = rectTransform.rect.width;
+        float th = rectTransform.rect.height;
+        float outerRadius = rectTransform.pivot.x * tw;
+        float innerRadius = rectTransform.pivot.x * tw - thickness;
+
+        Vector4 uv = overrideSprite != null ? DataUtility.GetOuterUV(overrideSprite) : Vector4.zero;
+
+        float uvCenterX = (uv.x + uv.z) * 0.5f;
+        float uvCenterY = (uv.y + uv.w) * 0.5f;
+        float uvScaleX = (uv.z - uv.x) / tw;
+        float uvScaleY = (uv.w - uv.y) / th;
+
+        float curDegree = 0;
+        UIVertex uiVertex;
+        int verticeCount;
+        int triangleCount;
+        Vector2 curVertice;
+
+        if (fill) //圆形
+        {
+            curVertice = Vector2.zero;
+            verticeCount = curSegements + 1;
+            uiVertex = new UIVertex();
+            uiVertex.color = color;
+            uiVertex.position = curVertice;
+            uiVertex.uv0 = new Vector2(curVertice.x * uvScaleX + uvCenterX, curVertice.y * uvScaleY + uvCenterY);
+            vh.AddVert(uiVertex);
+
+            for (int i = 1; i < verticeCount; i++)
+            {
+                float cosA = Mathf.Cos(curDegree);
+                float sinA = Mathf.Sin(curDegree);
+                curVertice = new Vector2(cosA * outerRadius, sinA * outerRadius);
+                curDegree += degreeDelta;
+
+                uiVertex = new UIVertex();
+                uiVertex.color = color;
+                uiVertex.position = curVertice;
+                uiVertex.uv0 = new Vector2(curVertice.x * uvScaleX + uvCenterX, curVertice.y * uvScaleY + uvCenterY);
+                vh.AddVert(uiVertex);
+
+                outterVertices.Add(curVertice);
+            }
+
+            triangleCount = curSegements * 3;
+            for (int i = 0, vIdx = 1; i < triangleCount - 3; i += 3, vIdx++)
+            {
+                vh.AddTriangle(vIdx, 0, vIdx + 1);
+            }
+            if (fillPercent == 1)
+            {
+                //首尾顶点相连
+                vh.AddTriangle(verticeCount - 1, 0, 1);
+            }
+        }
+        else//圆环
+        {
+            verticeCount = curSegements * 2;
+            for (int i = 0; i < verticeCount; i += 2)
+            {
+                float cosA = Mathf.Cos(curDegree);
+                float sinA = Mathf.Sin(curDegree);
+                curDegree += degreeDelta;
+
+                curVertice = new Vector3(cosA * innerRadius, sinA * innerRadius);
+                uiVertex = new UIVertex();
+                uiVertex.color = color;
+                uiVertex.position = curVertice;
+                uiVertex.uv0 = new Vector2(curVertice.x * uvScaleX + uvCenterX, curVertice.y * uvScaleY + uvCenterY);
+                vh.AddVert(uiVertex);
+                innerVertices.Add(curVertice);
+
+                curVertice = new Vector3(cosA * outerRadius, sinA * outerRadius);
+                uiVertex = new UIVertex();
+                uiVertex.color = color;
+                uiVertex.position = curVertice;
+                uiVertex.uv0 = new Vector2(curVertice.x * uvScaleX + uvCenterX, curVertice.y * uvScaleY + uvCenterY);
+                vh.AddVert(uiVertex);
+                outterVertices.Add(curVertice);
+            }
+
+            triangleCount = curSegements * 3 * 2;
+            for (int i = 0, vIdx = 0; i < triangleCount - 6; i += 6, vIdx += 2)
+            {
+                vh.AddTriangle(vIdx + 1, vIdx, vIdx + 3);
+                vh.AddTriangle(vIdx, vIdx + 2, vIdx + 3);
+            }
+            if (fillPercent == 1)
+            {
+                //首尾顶点相连
+                vh.AddTriangle(verticeCount - 1, verticeCount - 2, 1);
+                vh.AddTriangle(verticeCount - 2, 0, 1);
+            }
+        }
+
+    }
+
+    public virtual bool IsRaycastLocationValid(Vector2 screenPoint, Camera eventCamera)
+    {
+        Sprite sprite = overrideSprite;
+        if (sprite == null)
+            return true;
+
+        Vector2 local;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, screenPoint, eventCamera, out local);
+        return Contains(local, outterVertices, innerVertices);
+    }
+
+    private bool Contains(Vector2 p, List<Vector3> outterVertices, List<Vector3> innerVertices)
+    {
+        var crossNumber = 0;
+        RayCrossing(p, innerVertices, ref crossNumber);//检测内环
+        RayCrossing(p, outterVertices, ref crossNumber);//检测外环
+        return (crossNumber & 1) == 1;
+    }
+
+    /// <summary>
+    /// 使用RayCrossing算法判断点击点是否在封闭多边形里
+    /// </summary>
+    /// <param name="p"></param>
+    /// <param name="vertices"></param>
+    /// <param name="crossNumber"></param>
+    private void RayCrossing(Vector2 p, List<Vector3> vertices, ref int crossNumber)
+    {
+        for (int i = 0, count = vertices.Count; i < count; i++)
+        {
+            var v1 = vertices[i];
+            var v2 = vertices[(i + 1) % count];
+
+            //点击点水平线必须与两顶点线段相交
+            if (((v1.y <= p.y) && (v2.y > p.y))
+                || ((v1.y > p.y) && (v2.y <= p.y)))
+            {
+                //只考虑点击点右侧方向，点击点水平线与线段相交，且交点x > 点击点x，则crossNumber+1
+                if (p.x < v1.x + (p.y - v1.y) / (v2.y - v1.y) * (v2.x - v1.x))
+                {
+                    crossNumber += 1;
+                }
+            }
+        }
+    }
+
+
+
+    /// <summary>
+    /// Image's texture comes from the UnityEngine.Image.
+    /// </summary>
+    public override Texture mainTexture
+    {
+        get
+        {
+            return overrideSprite == null ? s_WhiteTexture : overrideSprite.texture;
+        }
+    }
+
+    public float pixelsPerUnit
+    {
+        get
+        {
+            float spritePixelsPerUnit = 100;
+            if (sprite)
+                spritePixelsPerUnit = sprite.pixelsPerUnit;
+
+            float referencePixelsPerUnit = 100;
+            if (canvas)
+                referencePixelsPerUnit = canvas.referencePixelsPerUnit;
+
+            return spritePixelsPerUnit / referencePixelsPerUnit;
+        }
+    }
+
+    // /// <summary>
+    // /// 子类需要重写该方法来自定义Image形状
+    // /// </summary>
+    // /// <param name="vh"></param>
+    // protected override void OnPopulateMesh(VertexHelper vh)
+    // {
+    //     base.OnPopulateMesh(vh);
+    // }
+
+    #region ISerializationCallbackReceiver
+    public void OnAfterDeserialize()
+    {
+    }
+
+    //
+    // 摘要: 
+    //     Implement this method to receive a callback after unity serialized your object.
+    public void OnBeforeSerialize()
+    {
+    }
+    #endregion
+
+    #region ILayoutElement
+    public virtual void CalculateLayoutInputHorizontal() { }
+    public virtual void CalculateLayoutInputVertical() { }
+
+    public virtual float minWidth { get { return 0; } }
+
+    public virtual float preferredWidth
+    {
+        get
+        {
+            if (overrideSprite == null)
+                return 0;
+            return overrideSprite.rect.size.x / pixelsPerUnit;
+        }
+    }
+
+    public virtual float flexibleWidth { get { return -1; } }
+
+    public virtual float minHeight { get { return 0; } }
+
+    public virtual float preferredHeight
+    {
+        get
+        {
+            if (overrideSprite == null)
+                return 0;
+            return overrideSprite.rect.size.y / pixelsPerUnit;
+        }
+    }
+
+    public virtual float flexibleHeight { get { return -1; } }
+
+    public virtual int layoutPriority { get { return 0; } }
+    #endregion
+
+    #region ICanvasRaycastFilter
+    // public virtual bool IsRaycastLocationValid(Vector2 screenPoint, Camera eventCamera)
+    // {
+    //     return true;
+    // }
+    #endregion
+
+
+
+}
+
+internal static class SetPropertyUtilityExt
+{
+    public static bool SetColor(ref Color currentValue, Color newValue)
+    {
+        if (currentValue.r == newValue.r && currentValue.g == newValue.g && currentValue.b == newValue.b && currentValue.a == newValue.a)
+            return false;
+
+        currentValue = newValue;
+        return true;
+    }
+
+    public static bool SetStruct<T>(ref T currentValue, T newValue) where T : struct
+    {
+        if (currentValue.Equals(newValue))
+            return false;
+
+        currentValue = newValue;
+        return true;
+    }
+
+    public static bool SetClass<T>(ref T currentValue, T newValue) where T : class
+    {
+        if ((currentValue == null && newValue == null) || (currentValue != null && currentValue.Equals(newValue)))
+            return false;
+
+        currentValue = newValue;
+        return true;
+    }
+}
---
+++ CircleSprite.cs
@@ -0,0 +1,120 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteAlways]
+//离线生成圆环 图片 缓存在预制体中
+public class CircleSprite : MonoBehaviour
+{
+    //空白区域色值
+    [Header("空白区域色值")]
+    public Color emptyColor = new Color(0, 0, 0, 0);
+    //圆环区域色值
+    [Header("圆环区域色值")]
+    public Color circleColor = Color.white;
+    //圆环内径/外径
+    [Header("圆环内径")]
+    public int minRadius = 40;
+    [Header("圆环外径[圆形尺寸]")]
+    public int maxRadius = 50;
+    //扇形角度
+    [Header("扇形角度")]
+    public float circleAngle = 90;
+
+    Color lastColor = Color.black;
+    Color lastCircleColor = Color.black;
+    int lastMinRadius = 0;
+    int lastMaxRadius = 0;
+    float lastCircleAngle = 0;
+
+    private void Update()
+    {
+        if (Application.isPlaying) return;
+        if (lastColor != emptyColor || lastCircleColor != circleColor || minRadius != lastMinRadius || maxRadius != lastMaxRadius || circleAngle != lastCircleAngle || circleAngle != lastCircleAngle)
+        {
+            circleAngle = Mathf.Clamp(circleAngle, 0, 361);
+            minRadius = minRadius > maxRadius ? maxRadius : minRadius;
+            lastColor = emptyColor;
+            lastCircleColor = circleColor;
+            lastMinRadius = minRadius;
+            lastMaxRadius = maxRadius;
+            lastCircleAngle = circleAngle;
+            var sprite = CreateSprite(minRadius, maxRadius, circleAngle / 2, circleColor);
+            sprite.name = "cirlce";
+            this.GetComponent<Image>().sprite = sprite;
+        }
+    }
+
+
+    /// <summary>
+    /// 绘制扇形圆环，生成Sprite
+    /// </summary>
+    /// <param name="minRadius">圆环内径，值为0即是实心圆</param>
+    /// <param name="maxRadius">圆环外径</param>
+    /// <param name="circleAngle">1/2扇形弧度，值>=180度即是整园</param>
+    Sprite CreateSprite(int minRadius, int maxRadius, float halfAngle, Color circleColor)
+    {
+        //图片尺寸
+        int spriteSize = maxRadius * 2;
+        //创建Texture2D
+        Texture2D texture2D = new Texture2D(spriteSize, spriteSize);
+        //图片中心像素点坐标
+        Vector2 centerPixel = new Vector2(spriteSize / 2, spriteSize / 2);
+        //
+        Vector2 tempPixel;
+        float tempAngle, tempDisSqr;
+        if (halfAngle > 0 && halfAngle < 360)
+        {
+            //遍历像素点，绘制扇形圆环
+            for (int x = 0; x < spriteSize; x++)
+            {
+                for (int y = 0; y < spriteSize; y++)
+                {
+                    //以中心作为起点，获取像素点向量
+                    tempPixel.x = x - centerPixel.x;
+                    tempPixel.y = y - centerPixel.y;
+                    //是否在半径范围内
+                    tempDisSqr = tempPixel.sqrMagnitude;
+                    if (tempDisSqr >= minRadius * minRadius && tempDisSqr <= maxRadius * maxRadius)
+                    {
+                        //是否在角度范围内
+                        tempAngle = Vector2.Angle(Vector2.up, tempPixel);
+                        if (tempAngle < halfAngle || tempAngle > 360 - halfAngle)
+                        {
+                            //设置像素色值
+                            texture2D.SetPixel(x, y, circleColor);
+                            continue;
+                        }
+                    }
+                    //设置为透明
+                    texture2D.SetPixel(x, y, emptyColor);
+                }
+            }
+        }
+        else
+        {
+            //遍历像素点，绘制圆环
+            for (int x = 0; x < spriteSize; x++)
+            {
+                for (int y = 0; y < spriteSize; y++)
+                {
+                    //以中心作为起点，获取像素点向量
+                    tempPixel.x = x - centerPixel.x;
+                    tempPixel.y = y - centerPixel.y;
+                    //是否在半径范围内
+                    tempDisSqr = tempPixel.sqrMagnitude;
+                    if (tempDisSqr >= minRadius * minRadius && tempDisSqr <= maxRadius * maxRadius)
+                    {
+                        //设置像素色值
+                        texture2D.SetPixel(x, y, circleColor);
+                        continue;
+                    }
+                    //设置为透明
+                    texture2D.SetPixel(x, y, emptyColor);
+                }
+            }
+        }
+        texture2D.Apply();
+        //创建Sprite
+        return Sprite.Create(texture2D, new Rect(0, 0, spriteSize, spriteSize), new Vector2(0.5f, 0.5f));
+    }
+}
